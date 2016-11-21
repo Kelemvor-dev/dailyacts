1aab434245cbc90ba0393379670ebd46ad2694a5
JGARCIA\Jeyson Garcia <jeysongar@gmail.com>
2016-11-21 09:29:38 -0500

    cambio de ambiente

---
+++ db/migrate/20161121142553_add_table_friends.rb
@@ -0,0 +1,10 @@
+class AddTableFriends < ActiveRecord::Migration
+  def change
+  	create_table "friends", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121135902) do
+ActiveRecord::Schema.define(version: 20161121142553) do
 
   create_table "friend_requests", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20161121135902) do
 
   add_index "friend_requests", ["friend_id"], name: "index_friend_requests_on_friend_id"
   add_index "friend_requests", ["user_id"], name: "index_friend_requests_on_user_id"
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
