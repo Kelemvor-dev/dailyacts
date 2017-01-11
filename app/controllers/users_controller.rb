cd20d2a2808b257d2072c2078c6552a76698ee7b
JGARCIA\Jeyson Garcia <jeysongar@gmail.com>
2017-01-11 10:52:36 -0500

    mostrando amigos en el perfil pendiente estilos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < ApplicationController
   
   def index
       @user = User.find(current_user.id)
-      @users = User.where.not("id = ?",current_user.id).order("first_name ASC") 
-      @outgoing = current_user.friend_requests           
+      @users = User.where.not("id = ?",current_user.id).order("first_name ASC")       
   end
   def show
+    @profiles = User.where("id in (select friend_id from friend_requests where user_id=?) and id not in (?)",current_user,current_user).order("first_name ASC")
   	@user_profile = User.find(params[:id])  
   	respond_to do |format|
         format.html # show.html.erb
